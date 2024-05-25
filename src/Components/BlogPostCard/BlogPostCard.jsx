974d1b49cb2ad4c052ba38e0b4201aa771dbc9d5
Pratyush 2nd github <155731201+pratyush0898@users.noreply.github.com>
2024-05-25 11:39:12 +0530

    Update BlogPostCard.jsx

--- src/Components/BlogPostCard/BlogPostCard.jsx
+++ src/Components/BlogPostCard/BlogPostCard.jsx
@@ -1,51 +1,21 @@
 import React, { useContext } from "react";
 import MyContext from "../../Context/data/MyContext";
-import DE2bGfs from '../../assets/DE2bGfs.jpeg'
+import BlogCard from "../BlogCard/BlogCard";
+import { Button } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 import "./BlogPostCard.css";
 
 const BlogPostCard = () => {
-  const context = useContext(MyContext);
-  const { mode, toggleMode } = context;
-
-  const BlogCard = ({childnum}) => {
-    return (
-      <>
-        <div id={"blog-card " + childnum}>
-          <div id="blog-card-image">
-            <img
-              src={DE2bGfs}
-              alt=""
-            />
-          </div>
-
-          <div id="blog-card-container">
-            <h3>DD Month YYYY</h3>
-            <h1 className={mode === "light" ? "card-black" : "card-white"}>
-              Heading
-            </h1>
-            <p className={mode === "light" ? "card-black" : "card-white"}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur ducimus sapiente cum in impedit laboriosam perferendis.
-              Porro nobis inventore, illum consequuntur veniam non, perspiciatis
-              quae, laborum ex dolores repellat dolorum.
-            </p>
-
-            <div id="blog-card-button">
-              <a href="#" target="_blank">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
   return (
     <>
-  <div id="blog-cards">
-    <BlogCard />
-    <BlogCard childnum="second"/>
-  </div>
+      <div id="blog-cards">
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+      </div>
+      <div id="See-more-blogs-button">
+        <Link to='/blogs'><Button>See more</Button></Link>
+      </div>
     </>
   );
 };
