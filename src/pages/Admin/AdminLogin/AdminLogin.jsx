48b087aa7f1b7950353ba0f608d8e98ae2bd3cdd
Pratyush 2nd github <155731201+pratyush0898@users.noreply.github.com>
2024-05-26 11:38:17 +0530

    Update AdminLogin.jsx

--- src/pages/Admin/AdminLogin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin/AdminLogin.jsx
@@ -1,12 +1,98 @@
-import React from 'react'
-import NavBar from '../../../Components/NavBar/NavBar'
+import React, { useContext, useState, useEffect } from "react";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Input,
+    Button,
+    Typography,
+} from "@material-tailwind/react";
+import MyContext from '../../../Context/data/MyContext';
+import './AdminLogin.css'
 
 const AdminLogin = () => {
-  return (
-    <>
-      <Navbar />
-    </>
-  )
-}
+    const context = useContext(MyContext);
+    const { mode } = context;
+
+    return (
+        <div className="flex justify-center items-center h-screen box-border">
+
+            {/* Card  */}
+            <Card
+                className="w-full max-w-[24rem]"
+                style={{
+                    background: mode === 'dark'
+                        ? 'rgb(30, 41, 59)'
+                        : 'rgb(226, 232, 240)'
+                }}
+            >
+                {/* CardHeader */}
+                <CardHeader
+                    color="blue"
+                    floated={false}
+                    shadow={false}
+                    className="m-0 grid place-items-center rounded-b-none py-8 px-4 text-center"
+                    style={{
+                        background: mode === 'dark'
+                            ? 'rgb(226, 232, 240)'
+                            : 'rgb(30, 41, 59)'
+                    }}
+                >
+                    <div className="mb-4 rounded-full border border-white/10 bg-white/10 p-2 text-white">
+                        <div className=" flex justify-center">
+                            {/* Image  */}
+                            <img src="https://cdn-icons-png.flaticon.com/128/727/727399.png" className="h-20 w-20"
+                            />
+                        </div>
+                    </div>
+
+                    {/* Top Haeding  */}
+                    <Typography variant="h4" style={{
+                        color: mode === 'dark'
+                            ? 'rgb(30, 41, 59)'
+                            : 'rgb(226, 232, 240)'
+                    }}>
+                        Admin Login
+                    </Typography>
+                </CardHeader>
+
+                {/* CardBody */}
+                <CardBody>
+                    <form className=" flex flex-col gap-4">
+                        {/* First Input  */}
+                        <div>
+                            <Input
+                                type="email"
+                                label="Email"
+                                name="email"
+                            />
+                        </div>
+                        {/* Second Input  */}
+                        <div>
+                            <Input
+                                type="password"
+                                label="Password"
+                            />
+                        </div>
+                        {/* Login Button  */}
+                        <Button
+                            style={{
+                                background: mode === 'dark'
+                                    ? 'rgb(226, 232, 240)'
+                                    : 'rgb(30, 41, 59)',
+                                color: mode === 'dark'
+                                    ? 'rgb(30, 41, 59)'
+                                    : 'rgb(226, 232, 240)'
+                            }}>
+                            Login
+                        </Button>
+                    </form>
+                </CardBody>
+            </Card>
+        </div>
+
+
+    );
+} 
 
 export default AdminLogin
